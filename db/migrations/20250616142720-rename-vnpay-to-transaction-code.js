c766fd90ec70e89d3cf2d42c456f570134703be5
vanh2753 <vanh2753@gmail.com>
2025-07-19 01:36:14 +0700

    fix article for mod

--- db/migrations/20250616142720-rename-vnpay-to-transaction-code.js
+++ db/migrations/20250616142720-rename-vnpay-to-transaction-code.js
@@ -1,12 +1,20 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.renameColumn("Orders", "vnpay_txn_code", "transaction_code");
+    await queryInterface.renameColumn(
+      "orders",
+      "vnpay_txn_code",
+      "transaction_code"
+    );
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.renameColumn("Orders", "transaction_code", "vnpay_txn_code");
-  }
+    await queryInterface.renameColumn(
+      "orders",
+      "transaction_code",
+      "vnpay_txn_code"
+    );
+  },
 };
